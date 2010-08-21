ca38cfb12f912d964f5011a204f8c7d5eae2f29f
darkness <darkness@linode.d1b.org>
2010-08-21 21:35:29 +1000

    update

---
+++ pythonz.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import urllib
+from lxml import html
+doc = html.fromstring(str(urllib.urlopen("http://vtr.aec.gov.au/HouseDownloadsMenu-15508-csv.htm").read()))
+
+z = [x.attrib['href'] for x in doc.xpath("//a[@href]") if x.text != None and "CSV" in str(x.text.encode("utf-8","ignore")).upper() ]
+for i in z:
+	url = "http://vtr.aec.gov.au/" + i
+	print url
+	urllib.urlretrieve(url ,i.replace("..", "").replace("Downloads/", "") )
+
