f9146d79268e0d171f883147606be39794ab3c6a
darkness <darkness@linode.d1b.org>
2010-08-21 23:34:01 +1000

    update

--- pythonz.py
+++ pythonz.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python
 import urllib
 from lxml import html
-doc = html.fromstring(str(urllib.urlopen("http://vtr.aec.gov.au/HouseDownloadsMenu-15508-csv.htm").read()))
 
-z = [x.attrib['href'] for x in doc.xpath("//a[@href]") if x.text != None and "CSV" in str(x.text.encode("utf-8","ignore")).upper() ]
-for i in z:
-	url = "http://vtr.aec.gov.au/" + i
-	print url
-	last_slash = i.rfind("/")
-	file = str(i[last_slash+1:])
-	file = file.replace("..", "").replace("Downloads/", "").replace("/", "")
-	print file
-	urllib.urlretrieve(url, file)
+
+def get_it(doc):
+	z = [x.attrib['href'] for x in doc.xpath("//a[@href]") if x.text != None and "CSV" in str(x.text.encode("utf-8","ignore")).upper() ]
+	for i in z:
+		url = "http://vtr.aec.gov.au/" + i
+		print url
+		last_slash = i.rfind("/")
+		file = str(i[last_slash+1:])
+		file = file.replace("..", "").replace("Downloads/", "").replace("/", "")
+		print file
+		urllib.urlretrieve(url, file)
+
+if __name__ == "__main__":
+	doc = html.fromstring(str(urllib.urlopen("http://vtr.aec.gov.au/HouseDownloadsMenu-15508-csv.htm").read()))
+	get_it(doc)
+	doc = html.fromstring(str(urllib.urlopen("http://vtr.aec.gov.au/SenateDownloadsMenu-15508-csv.htm").read()))
+	get_it(doc)
 
